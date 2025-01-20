ac57f4fb76aef3a484c17014ce0ad033fb112bb7
angelogentileiii <angelo.john.gentile@gmail.com>
2025-01-20 15:31:12 -0500

    updated blog practice site with db models and admin page

--- practice_site/blog/admin.py
+++ practice_site/blog/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
+from .models import Post, Author, Tag
+
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "date", "get_tags")
+
+    list_filter = ("author", "tags", "date")
+
+    prepopulated_fields = {"slug": ("title",)}
+
+    def get_tags(self, obj):
+        tags = obj.tags.all()
+        return ", ".join([tag.caption for tag in tags])
+    
+    get_tags.short_description = "Tags"
+    
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "get_posts")
+
+    list_filter = ("last_name",)
+
+    def get_posts(self, obj):
+        posts = obj.posts.all()
+        return ", ".join([post.title for post in posts])
+    
+    get_posts.short_description = "Posts"
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Tag)
--- practice_site/blog/models.py
+++ practice_site/blog/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+
 
 # Create your models here.
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    email_address = models.EmailField()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.caption
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    excerpt = models.CharField(max_length=250)
+    content = models.TextField(validators=[MinLengthValidator(25)])
+    date = models.DateField(auto_now=True)
+    image_name = models.CharField(max_length=25, )
+    slug = models.SlugField(unique=True)
+
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name="posts")
+
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return f'{self.title} by {self.author}'
