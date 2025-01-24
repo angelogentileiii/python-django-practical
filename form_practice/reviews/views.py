f6a33e20aa506c1937a0472dd33474b3db29ea31
angelogentileiii <angelo.john.gentile@gmail.com>
2025-01-24 16:51:06 -0500

    completed module on class views / updated form and reviews

--- form_practice/reviews/views.py
+++ form_practice/reviews/views.py
@@ -1,31 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.views import View
+from django.views.generic import ListView, DetailView
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import FormView, CreateView
 
 from .forms import ReviewForm
+from .models import Review
 
 # Create your views here.
 
-# Class Base View
-class ReviewView(View):
-    # We can build class view methods that represent HTTP requests --> Slightly more understandable/cleaner
-    def get(self, request):
-        form = ReviewForm()
+# Class Base View - FormView
+#   - The FormView handles the get method out of the box
+#   - We simply need to add the success_url for redirect on successful submission and a form_valid method
+#       - Within this method, we determine what we accomplish with the submitted data
+# class ReviewView(FormView):
+#     form_class = ReviewForm
+#     template_name = "reviews/review.html"
+#     success_url = "/thank-you"
 
-        return render(request, "reviews/review.html", {"form": form})
+#     def form_valid(self, form):
+#         form.save()
+#         return super().form_valid(form)
+    
+# Class Based View - CreateView
+#   - Will allow us to automatically create data in the database utilizing the submitted data
+#   - There are also UpdateViews and DeleteViews which work in a similar manner by integrating with the database
+class ReviewView(CreateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    success_url = "/thank-you"
 
-    def post(self, request):
-        form = ReviewForm(request.POST)
 
-        if form.is_valid():
-            print(form.cleaned_data)
+# Class Base View - Basic
+# class ReviewView(View):
+#     # We can build class view methods that represent HTTP requests --> Slightly more understandable/cleaner
+#     def get(self, request):
+#         form = ReviewForm()
 
-            # Because we are using a ModelForm, we can simply call save on the form to store into the inferred model table directly from the form
-            form.save()
+#         return render(request, "reviews/review.html", {"form": form})
 
-            return HttpResponseRedirect("/thank-you")
+#     def post(self, request):
+#         form = ReviewForm(request.POST)
 
-        return render(request, "reviews/review.html", {"form": form})
+#         if form.is_valid():
+#             print(form.cleaned_data)
+
+#             # Because we are using a ModelForm, we can simply call save on the form to store into the inferred model table directly from the form
+#             form.save()
+
+#             return HttpResponseRedirect("/thank-you")
+
+#         return render(request, "reviews/review.html", {"form": form})
 
 # Standard Function Based View
 # def review(request):
@@ -44,5 +71,35 @@ class ReviewView(View):
 
 #     return render(request, "reviews/review.html", {"form": form})
 
-def thank_you(request):
-    return render(request, "reviews/thank_you.html")
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["message"] = "This Works!"
+        return context
+    
+# ListView is a more specialized TemplateView for displaying a list of data
+class ReviewsListView(ListView):
+    template_name = "reviews/review_list.html"
+    model = Review
+    # Way to change the name of the data when it is exposed to the template
+    context_object_name = "reviews"
+
+    # Can limit or adjust our data queried from the DB
+    # def get_queryset(self):
+    #     base_query =  super().get_queryset()
+    #     data = base_query.filter(rating__gt=3)
+    #     return data
+
+# DetailView is a more specialized TemplateView for displaying the details of a single piece of data
+#   - Can find the item based on Primary Key or a Slug
+#   - It will pull the slug or pk from the dynamic url automatically
+# This detail view will pass the data to the template as a singular, lowercase version of the class name ("review" in this case) 
+#   - We can also extract values in the template by using "object"
+class ReviewDetailView(DetailView):
+    template_name = "reviews/review_details.html"
+    model = Review
+
+    
+    
